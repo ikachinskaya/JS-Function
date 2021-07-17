24029764b3e1f0811f445d0c893d9fa480aeb0ff
Inna <inna.kachinskaya13@gmail.com>
2021-07-18 01:06:44 +0300

    Add checkMultiplicity

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,11 @@ const isAdult = function (age) {
   return age >= 18 ? true : false;
 };
 
+const checkMultiplicity = function (number1, number2) {
+  if (isNaN(number1 - number2) || number1 === 0) {
+    return null;
+  }
+  return number1 % number2 === 0 ? true : false;
+};
+
+
