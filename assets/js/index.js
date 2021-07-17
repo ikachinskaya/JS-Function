2ea6e80f2c89ab0cd92fd76c2fbeda05cf2350a2
Inna <inna.kachinskaya13@gmail.com>
2021-07-18 01:09:58 +0300

    Add isTriangle

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,4 +12,13 @@ const checkMultiplicity = function (number1, number2) {
   return number1 % number2 === 0 ? true : false;
 };
 
+const isTriangle = function (a, b, c) {
+  if (isNaN(a - b - c) || a <= 0 || b <= 0 || c <= 0) {
+    return null;
+  }
+  if (a + b > c && a + c > b && b + c > a) {
+    return true;
+  }
+  return false;
+};
 
