d81e9a92c3b113c5557889e3615555847c5aa6b7
Inna <inna.kachinskaya13@gmail.com>
2021-07-21 15:29:02 +0300

    Fix index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,7 @@ const isTriangle = function (a, b, c) {
   if (isNaN(a - b - c) || a <= 0 || b <= 0 || c <= 0) {
     return null;
   }
-  if (a + b > c && a + c > b && b + c > a) {
-    return true;
-  }
-  return false;
+  return a + b > c && a + c > b && b + c > a;
 };
 
 const getAreaRhombus = function (a, h) {
